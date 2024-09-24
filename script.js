e8606cb1cf0d390ae9f9f17a4d9d41b207ac1f48
Hellhammer12345 <168800026+Hellhammer12345@users.noreply.github.com>
2024-09-24 16:34:25 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-function getAlert() {
-    alert ('Temur Bolkvadze')
-    
+let number = 10;
+
+if (number > 6) {
+    console.log(10);
+}else {
+    console.log("number less than 6");
 }
 
 
 
-function getCheck(params) {
+var first_name = "Temo"
+var last_name = "Bolkvadze"
+
+var first_name = "stalin"
+
+var age = 25
+
+console.log(first_name + " " + last_name);
+
+console.log(age);
 
 
-  
+let number_1 = 59
+let number_2 = 28
 
-    if(params>50) {
-        console.log('მეტია 50-ზე');
-    } else if (params < 50) {
-        console.log('ნაკლებია 50-ზე');
-    } else if (params == 50) {
-        console.log('ტოლია 50-ის');
-    }
-    
+let sum = number_1 * number_2
+console.log(sum);
+
+
+
+let car = {
+
+    make: "audi",
+    model: "a-10",
+    color: "white",
+    year: 2020,
+    price: 10000
+
 }
+
+console.log(car);
+
+
+
+
