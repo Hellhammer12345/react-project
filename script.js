e1e1c2e494d3b912b1716fa019dc34de06af8b5a
Hellhammer12345 <168800026+Hellhammer12345@users.noreply.github.com>
2024-09-24 16:23:25 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function getAlert() {
+    alert ('Temur Bolkvadze')
+    
+}
+
+
+
+function getCheck(params) {
+
+
+  
+
+    if(params>50) {
+        console.log('მეტია 50-ზე');
+    } else if (params < 50) {
+        console.log('ნაკლებია 50-ზე');
+    } else if (params == 50) {
+        console.log('ტოლია 50-ის');
+    }
+    
+}
